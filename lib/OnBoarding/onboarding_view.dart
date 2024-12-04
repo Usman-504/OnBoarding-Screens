7b6708e5eff384ed98fc1a7de9a5c1077a768951
Muhammad Usman <ahmadkhan75830@gmail.com>
2024-12-04 12:19:41 +0500

    Initial Commit

--- lib/OnBoarding/onboarding_view.dart
+++ lib/OnBoarding/onboarding_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:onboarding/home_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-
 import '../components/colors.dart';
 import 'onboarding_items.dart';
 
